67c1da0d199c8370a4287cc119d556377cd8e229
remax21 <deronzier.remi@gmail.com>
2022-08-26 17:08:25 +0200

    build: change server url

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ import { updateCactus, setupCactus, getCactusRects } from "./cactus.js";
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
 const SPEED_SCALE_INCREASE = 0.00001;
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://vodkasgard-jimy-phoenix-game.herokuapp.com";
 
 const worldElem = document.querySelector("[data-world]");
 const scoreElem = document.querySelector("[data-score]");
